6d1dada5925446cdd2e5d6cd3e5bac7eeb30d614
Andrew Lockwood <a.c.lockwood@gmail.com>
2024-04-06 18:56:45 -0400

    Update docker engine installer

--- scripts/bash/installers/docker-install.sh
+++ scripts/bash/installers/docker-install.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 set -e
+# NOTE: Installs Docker Engine, not Docker Desktop
+# Source: https://docs.docker.com/engine/install/ubuntu/
 
-#All this via: https://docs.docker.com/install/linux/docker-ce/ubuntu/
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# Cleanup
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get autoremove
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-#Fails if not found thanks to set -e
-sudo apt-key fingerprint 0EBFCD88 | grep "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-sudo docker run hello-world
-
+# Install Docker packages (latest)
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ## PERMISSIONS TO MANAGE AS NON-ROOT
-#Create docker group, add current user to it
-sudo groupadd docker
+#Create docker group if not already done, add current user to it
+if [ $(getent group docker) ]; then
+  echo "docker group exists"
+else
+   sudo groupadd docker
+fi
+
+#Add current user to docker group
 sudo usermod -aG docker $USER
 
 #Avoid logout & login to update group membership
