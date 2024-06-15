281a9f491d8924d2e09d02cd16051545566541d8
Andrew Lockwood <a.c.lockwood@gmail.com>
2024-06-15 11:05:12 -0400

    Update build-tools for pyenv builds

--- scripts/bash/system-config/build-tools.sh
+++ scripts/bash/system-config/build-tools.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# Should be enough to build Python from source (for pyenv)
 sudo apt update
-sudo apt install -y build-essential libedit-dev
+sudo apt install -y \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    curl \
+    git \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
