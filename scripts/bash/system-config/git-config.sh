6a964814901797da20b20b7fc4ebd073eea176f9
Andrew Lockwood <ALockwood@users.noreply.github.com>
2025-04-12 23:10:53 -0400

    [linuxGeartweaks] A Sad Default PR Title (#6)
    * git config tweak + make pr tweak
    
    * fix var format

--- scripts/bash/system-config/git-config.sh
+++ scripts/bash/system-config/git-config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+read -p "Enter your email address: " email
+if [ -z "$email" ]; then
+    echo "😞"
+    exit 1
+fi
+
 git config --global user.name "Andrew Lockwood"
-git config --global user.email "a.c.lockwood@gmail.com"
+git config --global user.email "${email}"
 git config --global core.editor "code"
